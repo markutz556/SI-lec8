9353e77e09b0c3c03721d218a256713f4b37239e
markutz556 <2917097440@qq.com>
2018-01-30 14:34:24 -0500

    With caching

--- ny-simple.py
+++ ny-simple.py
@@ -1,23 +1,61 @@
-
 #ny-simple.py
 from secrets import *
 import requests
+import json
+
+CACHE_FNAME = 'cache_file_name.json'
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    CACHE_DICTION = json.loads(cache_contents)
+    cache_file.close()
+
+# if there was no file, no worries. There will be soon!
+except:
+    CACHE_DICTION = {}
 
 # gets stories from a particular section of NY times
 def get_stories(section):
-    baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
-    extendedurl = baseurl + section + '.json'
-    params={'api-key': nyt_key}
-    return requests.get(extendedurl, params).json()
+	baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
+	extendedurl = baseurl + section + '.json'
+	params={'api-key': nyt_key}
+	return make_request_using_cache(extendedurl, params)
 
 def get_headlines(nyt_results_dict):
-    results = nyt_results_dict['results']
-    headlines = []
-    for r in results:
-        headlines.append(r['title'])
-    return headlines
+	results = nyt_results_dict['results']
+	headlines = []
+	for r in results:
+		headlines.append(r['title'])
+	return headlines
+
+def params_unique_combination(baseurl, params):
+    alphabetized_keys = sorted(params.keys())
+    res = []
+    for k in alphabetized_keys:
+        res.append("{}-{}".format(k, params[k]))
+    return baseurl + "_".join(res)
+
+def make_request_using_cache(baseurl, params):
+	unique_ident = params_unique_combination(baseurl,params)
+
+	## first, look in the cache to see if we already have this data
+	if unique_ident in CACHE_DICTION:
+		print("Getting cached data...")
+		return CACHE_DICTION[unique_ident]
+	
+	else:
+		print("Making a request for new data...")
+		# Make the request and cache the new data
+		resp = requests.get(baseurl, params)
+		CACHE_DICTION[unique_ident] = json.loads(resp.text)
+		dumped_json_cache = json.dumps(CACHE_DICTION)
+		fw = open(CACHE_FNAME,"w")
+		fw.write(dumped_json_cache)
+		fw.close() # Close the open file
+		return CACHE_DICTION[unique_ident]
+
 
 story_list_json = get_stories('science')
 headlines = get_headlines(story_list_json)
 for h in headlines:
-    print(h)
+	print(h)
